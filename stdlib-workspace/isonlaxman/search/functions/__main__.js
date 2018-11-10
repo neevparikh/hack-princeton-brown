dba8f01a6519b435a00cdf5c8aec1dc99b79a65f
Ryan Choi <ryan_choi@brown.edu>
2018-11-10 17:45:15 -0500

    files for laxman

--- stdlib-workspace/isonlaxman/search/functions/__main__.js
+++ stdlib-workspace/isonlaxman/search/functions/__main__.js
@@ -2,11 +2,11 @@ const PythonShell = require('python-shell');
 
 /**
 * A basic Hello World function
-* @param {string} city Who you're saying hello to
+* @param {string} zipcode Who you're saying hello to
 * @returns {string}
 */
-module.exports = (city = '', context, callback) => {
-  if (city == '') {
+module.exports = (zipcode = '', context, callback) => {
+  if (zipcode == '') {
     callback(null, JSON.stringify({
       status: 400,
       message: null
@@ -14,6 +14,18 @@ module.exports = (city = '', context, callback) => {
   } else {
     // Write code here to get the data
     // Use ajax requests to execute python requests? 
-    callback(null);
+    let options = {
+      pythonPath: '/Users/ryanchoi/miniconda3/bin/python', //replace with own
+      pythonOptions: ['-u'],
+      scriptPath: '../../../../python/',
+      args: ['zipcode']
+    };
+
+    PythonShell.run('./../../../neural-net.py', options, function(err, results) {
+    if(err) callback(null, err);
+    console.log('results: %j', results);
+    callback(null, results);
+    });
+
   }
 };
