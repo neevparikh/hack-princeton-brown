b38505faca62968f3ac5c6f61be209080db8a72a
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2018-11-10 18:20:23 -0500

    New way to make function calls
    Changed the function calls to stdlib to the asyn-await syntax

--- stdlib-workspace/isonlaxman/search/functions/__main__.js
+++ stdlib-workspace/isonlaxman/search/functions/__main__.js
@@ -1,4 +1,4 @@
-const PythonShell = require('python-shell');
+// const PythonShell = require('python-shell');
 
 /**
 * A basic Hello World function
@@ -6,6 +6,7 @@ const PythonShell = require('python-shell');
 * @returns {string}
 */
 module.exports = (zipcode = '', context, callback) => {
+
   if (zipcode == '') {
     callback(null, JSON.stringify({
       status: 400,
@@ -14,18 +15,21 @@ module.exports = (zipcode = '', context, callback) => {
   } else {
     // Write code here to get the data
     // Use ajax requests to execute python requests? 
-    let options = {
-      pythonPath: '/Users/ryanchoi/miniconda3/bin/python', //replace with own
-      pythonOptions: ['-u'],
-      scriptPath: '../../../../python/',
-      args: ['zipcode']
-    };
 
-    PythonShell.run('./../../../neural-net.py', options, function(err, results) {
-    if(err) callback(null, err);
-    console.log('results: %j', results);
-    callback(null, results);
-    });
+    console.log("HERE");
+    callback(null, "HELLO");
+    // let options = {
+    //   pythonPath: 'C:/Users/Laxman/AppData/Local/Programs/Python/Python37-32', //replace with own
+    //   pythonOptions: ['-u'],
+    //   scriptPath: '../../../../python/',
+    //   args: ['zipcode']
+    // };
+
+    // PythonShell.run('./../../../neural-net.py', options, function (err, results) {
+    //   if (err) callback(null, "ERROR" + JSON.stringify(err));
+    //   console.log('results: %j', results);
+    //   callback(null, JSON.stringify(results));
+    // });
 
   }
 };
