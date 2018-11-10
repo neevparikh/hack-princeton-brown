29c53a352ac002bd23b4722cb462a8db4b163e70
sonny3690 <sonny3690@gmail.com>
2018-11-10 16:28:51 -0500

    one hot encoding

--- python/zipcode_extracter.py
+++ python/zipcode_extracter.py
@@ -4,62 +4,106 @@ from uszipcode import SearchEngine
 def encoded_dict (zipcode, date):
         
         search = SearchEngine(simple_zipcode=False).by_zipcode (zipcode)
-        
-        keys_to_remove = search.keys()[:-20]
 
-        keys_to_remove.extend (['timezone', 'area_code_list', 'polygon', 'population_by_age', 'population_by_gender', 'population_by_race', 'head_of_household_by_age'])
+        #keys_to_remove.extend (['timezone', 'area_code_list', 'polygon', 'population_by_age', 'population_by_gender', 'population_by_race', 'head_of_household_by_age'])
+        keys_to_include = ["population_density",
+                            "population_by_year",
+                            "population_by_age",
+                            "population_by_gender",
+                            "population_by_race",
+                            "families_vs_singles",
+                            "households_with_kids",
+                            "median_household_income",
+                            "employment_status",
+                            "household_income",
+                            "educational_attainment_for_population_25_and_over",
+                            "school_enrollment_age_3_to_17"]
+
+
+        time_features = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+        "day1",
+        "day2",
+        "day3",
+        "day4",
+        "day5",
+        "day6",
+        "day7",
+        "day8",
+        "day9",
+        "day10",
+        "day11",
+        "day12",
+        "day13",
+        "day14",
+        "day15",
+        "day16",
+        "day17",
+        "day18",
+        "day19",
+        "day20",
+        "day21",
+        "day22",
+        "day23",
+        "day24",
+        "day25",
+        "day26",
+        "day27",
+        "day28",
+        "day29",
+        "day30",
+        "day31",
+        "year"
+        ]
         
         return_dict = {}
 
-
         for index, feature in enumerate (search.keys()):
-                if feature not in keys_to_remove:
-                        #return_dict[str(index) + feature] = search.values()[index]
-                        parent_key = str(index) + search.keys()[index]
-                        print (parent_key)
+                if feature in keys_to_include:
+                    #return_dict[str(index) + feature] = search.values()[index]
+                    parent_key = str(search.keys()[index])
+            
+                    #print (search.values()[index])
+                    if type(search.values()[index]) == list:
                         diction = list((search.values()[index])[0].values())[1]
-                        
-                        for each_dict in diction:
-                                combined_key = parent_key + ' - ' + each_dict['x']
-                                print (combined_key, each_dict['y'])
-                                return_dict[combined_key] = each_dict['y']
-                        
-                        
+                    else:
+                        return_dict [search.keys()[index]] = search.values()[index]
+                        continue
+                    
+                    for each_dict in diction:
+                            combined_key = parent_key + ' - ' + str(each_dict['x'])
+                            return_dict[combined_key] = each_dict['y']
 
-
-
-                        # for each_dict in search.values()[index]:                                
-                        #         #diction = list(each_dict.values())[1][0]
-                        #         diction = list(each_dict.values())[1][0]
-                        #         print (diction)
-                        #         values = list(diction.values())
-                                
-                        #         return_dict[values[0]] = values[1]
-
-                                
-                                #print(list(diction.values()))
-
-                        #print((search.values()[index]).keys())
-
-                        #print (search.get("Enrolled in Private School", "no animal available"))
-                        
-                                #print(search.values()[inde
-
-
-        print (return_dict)
-
-                                
-                
-        #print(return_dict[return_dict.keys()[]]
-        #print (return_dict['population_by_year'])
+        
 
         dates = date.split ('/')
-        date_array = [0] * 12
-        date_array[int(dates[0])-1] = 1
         
 
-        date_array = [0] * 12
-        date_array[int(dates[0])-1] = 1
-
-        print(date_array)
+        for index, time in enumerate (time_features):
+            return_dict[time_features[index]] = 0
         
+            if (index == int (dates[0])-1):
+                return_dict[time_features[index]] = 1
+            elif (index == int(dates[1])+11):
+                return_dict[time_features[index]] = 1
+
+        return_dict["year"] = int(dates[2])
+
+        
+        print (return_dict)
+
+
+            
+
+encoded_dict ('07450', '10/21/2018')
