0977616b6e4e0ef4e0b961cc6b1418bb9e273c65
isonlaxman <32971829+isonlaxman@users.noreply.github.com>
2018-11-10 22:22:52 -0500

    Some map changes

--- website/js/main.js
+++ website/js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// Header Scroll
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var scroll = $(window).scrollTop();
 
-		if (scroll >= 50) {
-			$('#header').addClass('fixed');
-		} else {
-			$('#header').removeClass('fixed');
-		}
+		// if (scroll >= 50) {
+		// $('#header').addClass('fixed');
+		// } else {
+		$('#header').removeClass('fixed');
+		// }
 	});
 
 	// Fancybox
@@ -21,35 +21,35 @@ $(document).ready(function() {
 
 	// Page Scroll
 	var sections = $('section')
-		nav = $('nav[role="navigation"]');
+	nav = $('nav[role="navigation"]');
 
 	$(window).on('scroll', function () {
-	  	var cur_pos = $(this).scrollTop();
-	  	sections.each(function() {
-	    	var top = $(this).offset().top - 76
-	        	bottom = top + $(this).outerHeight();
-	    	if (cur_pos >= top && cur_pos <= bottom) {
-	      		nav.find('a').removeClass('active');
-	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-	    	}
-	  	});
+		var cur_pos = $(this).scrollTop();
+		sections.each(function () {
+			var top = $(this).offset().top - 76
+			bottom = top + $(this).outerHeight();
+			if (cur_pos >= top && cur_pos <= bottom) {
+				nav.find('a').removeClass('active');
+				nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+			}
+		});
 	});
 	nav.find('a').on('click', function () {
-	  	var $el = $(this)
-	    	id = $el.attr('href');
+		var $el = $(this)
+		id = $el.attr('href');
 		$('html, body').animate({
 			scrollTop: $(id).offset().top - 75
 		}, 500);
-	  return false;
+		return false;
 	});
 
 	// Mobile Navigation
-	$('.nav-toggle').on('click', function() {
+	$('.nav-toggle').on('click', function () {
 		$(this).toggleClass('close-nav');
 		nav.toggleClass('open');
 		return false;
-	});	
-	nav.find('a').on('click', function() {
+	});
+	nav.find('a').on('click', function () {
 		$('.nav-toggle').toggleClass('close-nav');
 		nav.toggleClass('open');
 	});
